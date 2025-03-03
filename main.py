b4abb221b6cd5f392ec8afd99aa753ee0fedbf67
mxdeley <dev@madeleydesignstudio.org>
2025-03-03 21:07:20 +0000

    added triangle

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import pygame
 from constants import *
+from player import Player
 
 
-pygame.init()
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
-running = True
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
-    screen.fill((0, 0, 0))
 
-    pygame.display.flip()
-
-pygame.quit()
+if __name__ == "__main__":
+    main()
