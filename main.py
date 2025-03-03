d8aea6122f44a394979b0aea69b26847d3d9489b
mxdeley <dev@madeleydesignstudio.org>
2025-03-03 20:41:02 +0000

    added starter code

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+from constants import *
+
+
+pygame.init()
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))
+
+    pygame.display.flip()
+
+pygame.quit()
